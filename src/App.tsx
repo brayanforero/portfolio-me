168bf9088cf2f3824b62552eba7c43b1772e5602
Brayan Forero <Brayan Forero>
2022-06-12 13:14:10 -0400

    project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hola mundo 1</h1>
+      <div className="alert">
+        <b>Alert !!</b>
+      </div>
+    </>
+  )
+}
+
+export default App
