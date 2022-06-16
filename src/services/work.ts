b38f25b9e5693f8100ff6c0c03e1281d410376be
Brayan Forero <Brayan Forero>
2022-06-16 11:52:01 -0400

    feat:(root) add api url in work services

--- src/services/work.ts
+++ src/services/work.ts
@@ -1,7 +1,7 @@
 import { Work } from '../types/models/core'
-
+const URL_API = 'https://api-porftfolio.up.railway.app/api'
 export const getWorks = async (): Promise<Work[]> => {
-  const res = await fetch('/db.json')
+  const res = await fetch(URL_API)
   const { items } = await res.json()
   return items as Work[]
 }
