7f930d511207cfb544c4b1b21c0a479fa7e56993
Brayan Forero <bforero.engineer@gmail.com>
2024-10-08 13:02:51 -0400

    update:(root) fixing widget component

--- src/components/ImWorking/ImWorking.tsx
+++ src/components/ImWorking/ImWorking.tsx
@@ -1,16 +1,15 @@
 import './ImWorking.css'
 
-function ImWorking (): JSX.Element {
+function ImWorking(): JSX.Element {
   return (
     <main className="Imworking">
       <section className="p-5 rounded-lg w-xs h-72">
         <div className="flex flex-col items-center gap-4">
           <div className="px-4 py-6 fire">
-            <span className="mb-4 transition-all text-9xl">🔥</span>
+            <span className="mb-4 transition-all text-9xl">🚧</span>
           </div>
           <p className="text-2xl font-semibold">
-            Sorry. I am fixing some things, but do not you worry, soon I come
-            back.
+            Hey, I'm improving this website. No worries, I'll see you soon.
           </p>
         </div>
       </section>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
-import { LanguageProvider } from './context'
+import { ImWorking } from './components'
 import './styles/index.css'
 
 const rootDiv = document.getElementById('root')
@@ -11,10 +10,8 @@ if (rootDiv == null) {
   document.body.innerHTML = '<h1>No provided root element</h1>'
 } else {
   ReactDOM.createRoot(rootDiv).render(
-  <React.StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+      <ImWorking />
+    </React.StrictMode>
   )
 }
