fdee0c183f6c9142d8aee23d2b649a027becf489
Brayan Forero <bforero.engineer@gmail.com>
2024-10-14 14:07:17 -0400

    add(root): new header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,19 @@
+import useTransparency from "@/hooks/utilities/useTransparency"
+
+
+function Header() {
+  const { headerRef, isFar } = useTransparency({ distance: 100 })
+  const classEffect = isFar ? 'backdrop-blur-sm bg-purple-400/10 outline-purple-500'
+    : 'backdrop-blur-0 bg-purple-300/0 outline-purple-500/0'
+  return (
+    <nav ref={headerRef} className={`fixed top-4 left-0 right-0 z-10 flex items-center justify-center p-3 transition-colors w-[20rem] md:w-[26.25rem] gap-5 mx-auto text-base md:text-lg font-semibold rounded-full outline-1 outline ${classEffect}`}>
+      <a className="transition-colors hover:text-purple-500" href="#top">Inicio {isFar}
+      </a>
+      <a className="hover:text-purple-500" href="#experience">Experiencia</a>
+      <a className="hover:text-purple-500" href="#projects">Proyectos</a>
+      <a className="hover:text-purple-500" href="#me">Sobre Mi</a>
+    </nav>
+  )
+}
+
+export default Header
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootDiv = document.getElementById('root')
+
+
+if (rootDiv === null) { document.body.innerHTML = `<h1>Root Element has not provided</h1>` }
+
+else {
+  createRoot(rootDiv).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
